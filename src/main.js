6a9ce6565044e423a2cce395efe7d8d357f666fc
Appwrite <team@appwrite.io>
2024-05-14 16:49:27 +0000

    Create 'Prompt ChatGPT' function

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import OpenAI from 'openai';
+import { getStaticFile, throwIfMissing } from './utils.js';
+
+export default async ({ req, res }) => {
+  throwIfMissing(process.env, ['OPENAI_API_KEY']);
+
+  if (req.method === 'GET') {
+    return res.send(getStaticFile('index.html'), 200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+  }
+
+  try {
+    throwIfMissing(req.body, ['prompt']);
+  } catch (err) {
+    return res.json({ ok: false, error: err.message }, 400);
+  }
+
+  const openai = new OpenAI();
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? '512'),
+      messages: [{ role: 'user', content: req.body.prompt }],
+    });
+    const completion = response.choices[0].message.content;
+    return res.json({ ok: true, completion }, 200);
+  } catch (err) {
+    return res.json({ ok: false, error: 'Failed to query model.' }, 500);
+  }
+};
